c094a0de38d93d73c25c0664fa600251391a3f15
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-04-20 14:15:21 +0200

    added structure, styling and moving

---
+++ app.js
@@ -0,0 +1,31 @@
+const timeLeftDisplay = document.querySelector('#time-left');
+const resultDisplay = document.querySelector('#result');
+const startPauseButton = document.querySelector('#start-pause-button');
+const squares = document.querySelectorAll('.grid div');
+let currentIndex = 76;
+const width = 9;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove('frog');
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case 'ArrowRight':
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case 'ArrowUp':
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case 'ArrowDown':
+      if (currentIndex + width <= width * width) currentIndex += width;
+      break;
+
+    default:
+      break;
+  }
+  squares[currentIndex].classList.add('frog');
+}
+
+document.addEventListener('keydown', moveFrog);
